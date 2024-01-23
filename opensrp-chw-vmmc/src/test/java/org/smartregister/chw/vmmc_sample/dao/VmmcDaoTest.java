b52b1e37c4ff24db8a9a379006b80a7ed1f67a08
cozej4 <ilakozejumanne@gmail.com>
2024-01-23 10:10:00 +0300

    :hammer: Added some more Unit Tests

--- opensrp-chw-vmmc/src/test/java/org/smartregister/chw/vmmc_sample/dao/VmmcDaoTest.java
+++ opensrp-chw-vmmc/src/test/java/org/smartregister/chw/vmmc_sample/dao/VmmcDaoTest.java
@@ -13,6 +13,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.smartregister.chw.vmmc.dao.VmmcDao;
 import org.smartregister.repository.Repository;
 
+import java.util.Date;
+
 @RunWith(MockitoJUnitRunner.class)
 public class VmmcDaoTest extends VmmcDao {
 
@@ -29,11 +31,51 @@ public class VmmcDaoTest extends VmmcDao {
     }
 
     @Test
-    public void testIsRegisteredForMalaria() {
+    public void testIsRegisteredForVmmc() {
         Mockito.doReturn(database).when(repository).getReadableDatabase();
         boolean registered = VmmcDao.isRegisteredForVmmc("12345");
-        Mockito.verify(database).rawQuery(Mockito.anyString(), Mockito.any());
+        Mockito.verify(database).rawQuery(Mockito.contains("SELECT count(p.base_entity_id) count FROM ec_vmmc_enrollment p WHERE p.base_entity_id = '12345' AND p.is_closed = 0"), Mockito.any());
         Assert.assertFalse(registered);
+    }
+
+    @Test
+    public void testGetVmmcTestDate() {
+        Mockito.doReturn(database).when(repository).getReadableDatabase();
+        Date testDate = VmmcDao.getVmmcTestDate("34233");
+        Mockito.verify(database).rawQuery(Mockito.contains("select vmmc_test_date from ec_vmmc_enrollment where base_entity_id = '34233'"), Mockito.any());
+    }
+
+    @Test
+    public void testGetGentialExamination() {
+        Mockito.doReturn(database).when(repository).getReadableDatabase();
+        String result = VmmcDao.getGentialExamination("45678");
+        Mockito.verify(database).rawQuery(Mockito.contains("SELECT genital_examination FROM ec_vmmc_services p  WHERE p.entity_id = '45678' ORDER BY last_interacted_with DESC LIMIT 1"), Mockito.any());
+        Assert.assertNotNull(result);
+    }
+
+
+    @Test
+    public void testGetSystolic() {
+        Mockito.doReturn(database).when(repository).getReadableDatabase();
+        String result = VmmcDao.getSystolic("78901");
+        Mockito.verify(database).rawQuery(Mockito.contains("SELECT systolic FROM ec_vmmc_services p  WHERE p.entity_id = '78901' ORDER BY last_interacted_with DESC LIMIT 1"), Mockito.any());
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testGetDiastolic() {
+        Mockito.doReturn(database).when(repository).getReadableDatabase();
+        String result = VmmcDao.getDiastolic("23456");
+        Mockito.verify(database).rawQuery(Mockito.contains("SELECT diastolic FROM ec_vmmc_services p  WHERE p.entity_id = '23456' ORDER BY last_interacted_with DESC LIMIT 1"), Mockito.any());
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testGetAnyComplaints() {
+        Mockito.doReturn(database).when(repository).getReadableDatabase();
+        String result = VmmcDao.getAnyComplaints("56789");
+        Mockito.verify(database).rawQuery(Mockito.contains("SELECT any_complaints FROM ec_vmmc_services p  WHERE p.entity_id = '56789' ORDER BY last_interacted_with DESC LIMIT 1"), Mockito.any());
+        Assert.assertNotNull(result);
     }
 }
 
